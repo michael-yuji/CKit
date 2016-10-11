a5d1013b564a227cfc0fab38906c3e0839b8757d
yuuji <hatsuneyuji@yahoo.co.j>
2016-10-11 00:17:54 -0700

    errno

---
+++ Sources/errno.swift
@@ -0,0 +1,19 @@
+
+#if os(OSX) || os(iOS) || os(tvOS) || os(watchOS)
+import Darwin
+#else
+import Glibc
+#endif
+
+extension String {
+    public static var lastErrnoString: String {
+        return errnoString(errno)
+    }
+    
+    public static func errnoString(_ n: Int32) -> String {
+        guard let cErrStr = strerror(n) else {
+            return "Cannot get error string from errno"
+        }
+        return String(cString: cErrStr)
+    }
+}
