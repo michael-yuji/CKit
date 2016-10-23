d27c73c90feb9ec8625a976977685389156ae1aa
yuuji <hatsuneyuji@yahoo.co.j>
2016-10-23 06:53:05 -0700

    added now() to timespec

--- Sources/Date.swift
+++ Sources/Date.swift
@@ -26,6 +26,27 @@ extension timespec : Hashable, Equatable, Comparable {
     static var distantFuture: timespec {
         return timespec(tv_sec: Int.max, tv_nsec: Int.max)
     }
+    
+    public static func now() -> timespec {
+        var time = timespec()
+        #if os(OSX) || os(iOS) || os(tvOS) || os(watchOS)
+            if #available(OSX 10.12, iOS 10, *) {
+                clock_gettime(_CLOCK_REALTIME, &time)
+            } else {
+                var clock = clock_serv_t()
+                host_get_clock_service(mach_host_self(), CALENDAR_CLOCK, &clock)
+                var mach_ts = mach_timespec()
+                clock_get_time(clock, &mach_ts)
+                time = timespec(tv_sec: Int(mach_ts.tv_sec), tv_nsec: Int(mach_ts.tv_nsec))
+                mach_port_deallocate(mach_task_self_, clock)
+            }
+        #elseif os(FreeBSD)
+            clock_gettime(CLOCK_REALTIME_FAST, &time)
+        #elseif os(Linux)
+            clock_gettime(CLOCK_REALTIME, &time)
+        #endif
+        return time
+    }
 
     public var hashValue: Int {
         return self.tv_nsec + self.tv_sec * 1_000_000_000
