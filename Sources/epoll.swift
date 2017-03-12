d752263797660f9dfe0124f753337343a48d363b
Yuji <hatsuneyuji@yahoo.co.jp>
2017-03-11 19:49:38 -0800

    clean up

--- Sources/epoll.swift
+++ Sources/epoll.swift
@@ -33,106 +33,6 @@
 public struct Epoll: FileDescriptorRepresentable {
     public var fileDescriptor: Int32
     
-    public struct EpollEvents : OptionSet {
-        public typealias RawValue = UInt32
-        public var rawValue: UInt32
-        
-        public init(rawValue: UInt32) {
-            self.rawValue = rawValue
-        }
-        
-        public init(rawValue: Int32) {
-            self.rawValue = UInt32(rawValue)
-        }
-        
-        /// The associated file is available for `read` operations
-        public static let pollin = EpollEvents(rawValue: EPOLLIN.rawValue)
-        
-        /// The associated file is available for `write` operations
-        public static let pollout = EpollEvents(rawValue: EPOLLOUT.rawValue)
-        
-        /// Stream socket peer closed connection, or shutdown writing
-        /// half of connection. (This flag is especially useful for
-        /// writing simple code to detect peer shutdown when using 
-        /// Edge Triggered monitoring.
-        public static let rdhup = EpollEvents(rawValue: EPOLLRDHUP.rawValue)
-        
-        /// There is urgent data available for `read` operations
-        public static let pri = EpollEvents(rawValue: EPOLLPRI.rawValue)
-        
-        /// Error condition happened on the associated file descriptor.
-        /// `epoll_wait` will always wait for this event; it is not 
-        /// necessary to set it in `events`
-        public static let err = EpollEvents(rawValue: EPOLLERR.rawValue)
-        
-        /// Hang up happened on the associated file descriptor.
-        /// `epoll_wait` will always wait for this event; it is not
-        /// necessary to set it in `events`. Note that when reading
-        /// from a channel such as a pipe or a stream socket, this
-        /// event merely indicates that the peer closed its end of 
-        /// channel. Subsequent reads from the channel will return 
-        /// 0 (end of file) only after all outstanding data in the 
-        /// channel has been consumed.
-        public static let hup = EpollEvents(rawValue: EPOLLHUP.rawValue)
-        
-        /// Sets the Edge Triggered behavior for the associated file
-        /// descriptor. The default behavior for `epoll` is Level
-        /// Triggered. See `epoll` for more detailed information
-        /// about Edge and Level Triggered event distribution
-        /// architectures.
-        public static let edgeTrigger = EpollEvents(rawValue: EPOLLET.rawValue)
-    
-        /// Sets the one-shot behavior for the associated file descriptor.
-        /// This means that after an event is pulled out with `epoll_wait`
-        /// the associated file descriptor is internally disabled and
-        /// no other events will be reported by the `epoll` interface. 
-        /// The user much call `epoll_ctl()` with `EPOLL_CTL_MOD` to
-        /// rearm the file descriptor with a new event mask.
-        public static let oneshot = EpollEvents(rawValue: EPOLLONESHOT.rawValue)
-        
-        /// If `EPOLLONESHOT` and `EPOLLET` are clear and the process
-        /// has `CAP_BLOCK_SUSPEND` capability, ensure that the system
-        /// does not enter "suspend" or "hibernate" while this event
-        /// is pending or being processed. The event is considered as
-        /// being "processed" from the time when it is returned by a 
-        /// call to `epoll_wait` descriptor, the closure of that file
-        /// descriptor, the removal of the event file descriptor with
-        /// `EPOLL_CTL_DEL` or the clearing of `EPOLLWAKEUP` for the 
-        /// event file descriptor with `EPOLL_CTL_MOD`. See also BUGS.
-        public static let wakeup = EpollEvents(rawValue: EPOLLWAKEUP.rawValue)
-        
-        /// Sets an exclusive wakeup mode for the epoll file descriptor
-        /// this is being attached to the target file descriptor, `fd`.
-        /// When a wakeup event occurs and multiple epoll file descriptors
-        /// are attached to the same target file using `EPOLLEXCLUSIVE`,
-        /// one or more of the epoll file descriptors will receive an
-        /// event with `epoll_wait`. The default in this scenario (when
-        /// `EPOLLEXCLUSIVE` ts not set) is for all epoll file descriptors
-        /// to receive an event. `EPOLLEXCLUSIVE` is thus useful for
-        /// avoiding thundering herd problems in certain scenarios.
-        ///
-        /// If the same file descriptor is in multiple epoll instances, 
-        /// some with the `EPOLLEXCLUSIVE` flag, and others without, then
-        /// events will be provided to all epoll instances that did not
-        /// specify `EPOLLEXCLUSIVE`, and at least one of the epoll 
-        /// instances that did specify `EPOLLEXCLUSIVE`.
-        ///
-        /// The following values may be specified in conjunction with
-        /// `EPOLLEXCLUSIVE: EPOLLIN, EPOLLOUT, EPOLLWAKEUP`, and `EPOLLET`.
-        /// EPOLLHUP and EPOLLERR can also be specified, but this is not
-        /// required: as usual, these events are always reported if they
-        /// occur, regardless of whether they are specified in events.
-        /// Attempts to specify other values in `events` yield an error.
-        /// `EPOLLEXCLUSIVE` may be used only in an `EPOLL_CTL_ADD`
-        /// operation; attempts to employ it with `EPOLL_CTL_MOD` yield an
-        /// error. If `EPOLLEXCLUSIVE` has been set usng `epoll_ctl()`,
-        /// then a subsequent `EPOLL_CTL_MOD` on the same `epfd`, `fd` pair
-        /// yields an error. A call to `epoll_ctl()` that specifies 
-        /// `EPOLLEXCLUSIVE` in `events` and specifies the target file 
-        /// descriptor `fd` as an epoll instance will likewise fail. The
-        /// error in all of these cases is EINVAL
-//        public static let exclusive = EpollEvents(rawValue: EPOLLEXCLUSIVE)
-    }
     
     public func add(fd: Int32, for events: EpollEvents) {
         var ev = epoll_event(events: events.rawValue, data: epoll_data_t(fd: fd)) // to use pointer
@@ -152,4 +52,104 @@ public struct Epoll: FileDescriptorRepresentable {
     public init() {
         fileDescriptor = epoll_create(1024)
     }
+}
+public struct EpollEvents : OptionSet {
+    public typealias RawValue = UInt32
+    public var rawValue: UInt32
+    
+    public init(rawValue: UInt32) {
+        self.rawValue = rawValue
+    }
+    
+    public init(rawValue: Int32) {
+        self.rawValue = UInt32(rawValue)
+    }
+    
+    /// The associated file is available for `read` operations
+    public static let pollin = EpollEvents(rawValue: EPOLLIN.rawValue)
+    
+    /// The associated file is available for `write` operations
+    public static let pollout = EpollEvents(rawValue: EPOLLOUT.rawValue)
+    
+    /// Stream socket peer closed connection, or shutdown writing
+    /// half of connection. (This flag is especially useful for
+    /// writing simple code to detect peer shutdown when using
+    /// Edge Triggered monitoring.
+    public static let rdhup = EpollEvents(rawValue: EPOLLRDHUP.rawValue)
+    
+    /// There is urgent data available for `read` operations
+    public static let pri = EpollEvents(rawValue: EPOLLPRI.rawValue)
+    
+    /// Error condition happened on the associated file descriptor.
+    /// `epoll_wait` will always wait for this event; it is not
+    /// necessary to set it in `events`
+    public static let err = EpollEvents(rawValue: EPOLLERR.rawValue)
+    
+    /// Hang up happened on the associated file descriptor.
+    /// `epoll_wait` will always wait for this event; it is not
+    /// necessary to set it in `events`. Note that when reading
+    /// from a channel such as a pipe or a stream socket, this
+    /// event merely indicates that the peer closed its end of
+    /// channel. Subsequent reads from the channel will return
+    /// 0 (end of file) only after all outstanding data in the
+    /// channel has been consumed.
+    public static let hup = EpollEvents(rawValue: EPOLLHUP.rawValue)
+    
+    /// Sets the Edge Triggered behavior for the associated file
+    /// descriptor. The default behavior for `epoll` is Level
+    /// Triggered. See `epoll` for more detailed information
+    /// about Edge and Level Triggered event distribution
+    /// architectures.
+    public static let edgeTrigger = EpollEvents(rawValue: EPOLLET.rawValue)
+    
+    /// Sets the one-shot behavior for the associated file descriptor.
+    /// This means that after an event is pulled out with `epoll_wait`
+    /// the associated file descriptor is internally disabled and
+    /// no other events will be reported by the `epoll` interface.
+    /// The user much call `epoll_ctl()` with `EPOLL_CTL_MOD` to
+    /// rearm the file descriptor with a new event mask.
+    public static let oneshot = EpollEvents(rawValue: EPOLLONESHOT.rawValue)
+    
+    /// If `EPOLLONESHOT` and `EPOLLET` are clear and the process
+    /// has `CAP_BLOCK_SUSPEND` capability, ensure that the system
+    /// does not enter "suspend" or "hibernate" while this event
+    /// is pending or being processed. The event is considered as
+    /// being "processed" from the time when it is returned by a
+    /// call to `epoll_wait` descriptor, the closure of that file
+    /// descriptor, the removal of the event file descriptor with
+    /// `EPOLL_CTL_DEL` or the clearing of `EPOLLWAKEUP` for the
+    /// event file descriptor with `EPOLL_CTL_MOD`. See also BUGS.
+    public static let wakeup = EpollEvents(rawValue: EPOLLWAKEUP.rawValue)
+    
+    /// Sets an exclusive wakeup mode for the epoll file descriptor
+    /// this is being attached to the target file descriptor, `fd`.
+    /// When a wakeup event occurs and multiple epoll file descriptors
+    /// are attached to the same target file using `EPOLLEXCLUSIVE`,
+    /// one or more of the epoll file descriptors will receive an
+    /// event with `epoll_wait`. The default in this scenario (when
+    /// `EPOLLEXCLUSIVE` ts not set) is for all epoll file descriptors
+    /// to receive an event. `EPOLLEXCLUSIVE` is thus useful for
+    /// avoiding thundering herd problems in certain scenarios.
+    ///
+    /// If the same file descriptor is in multiple epoll instances,
+    /// some with the `EPOLLEXCLUSIVE` flag, and others without, then
+    /// events will be provided to all epoll instances that did not
+    /// specify `EPOLLEXCLUSIVE`, and at least one of the epoll
+    /// instances that did specify `EPOLLEXCLUSIVE`.
+    ///
+    /// The following values may be specified in conjunction with
+    /// `EPOLLEXCLUSIVE: EPOLLIN, EPOLLOUT, EPOLLWAKEUP`, and `EPOLLET`.
+    /// EPOLLHUP and EPOLLERR can also be specified, but this is not
+    /// required: as usual, these events are always reported if they
+    /// occur, regardless of whether they are specified in events.
+    /// Attempts to specify other values in `events` yield an error.
+    /// `EPOLLEXCLUSIVE` may be used only in an `EPOLL_CTL_ADD`
+    /// operation; attempts to employ it with `EPOLL_CTL_MOD` yield an
+    /// error. If `EPOLLEXCLUSIVE` has been set usng `epoll_ctl()`,
+    /// then a subsequent `EPOLL_CTL_MOD` on the same `epfd`, `fd` pair
+    /// yields an error. A call to `epoll_ctl()` that specifies
+    /// `EPOLLEXCLUSIVE` in `events` and specifies the target file
+    /// descriptor `fd` as an epoll instance will likewise fail. The
+    /// error in all of these cases is EINVAL
+    //        public static let exclusive = EpollEvents(rawValue: EPOLLEXCLUSIVE)
 }
--- Sources/kevent.swift
+++ Sources/kevent.swift
@@ -30,8 +30,29 @@
 //  Copyright © 2017 Yuji. All rights reserved.
 //
 
-#if !os(Linux)
-public typealias KernelEvent = xlibc.kevent
+public typealias KernelEvent = kevent
+
+#if os(Linux)
+public typealias KernelEvent = kevent
+public struct kevent {
+    
+    public var ident: UInt /* identifier for this event */
+    
+    public var filter: Int16 /* filter for event */
+    
+    public var flags: UInt16 /* general flags */
+    
+    public var fflags: UInt32 /* filter-specific flags */
+    
+    public var data: Int /* filter-specific data */
+    
+    public var udata: UnsafeMutableRawPointer! /* opaque user data identifier */
+    
+    public init()
+    
+    public init(ident: UInt, filter: Int16, flags: UInt16, fflags: UInt32, data: Int, udata: UnsafeMutableRawPointer!)
+}
+#else
 
 public protocol KernelEventFlags {
     var rawValue: UInt32 {get}
@@ -74,14 +95,7 @@ extension KernelEventDescriptor {
         
         typealias T = KernelEventType
         
-        self.type =
-            event.filter == T.read.rawValue ? .read :
-            event.filter == T.write.rawValue ? .write :
-            event.filter == T.signal.rawValue ? .signal :
-            event.filter == T.vnode.rawValue ? .vnode :
-            event.filter == T.timer.rawValue ? .timer :
-            event.filter == T.proc.rawValue ? .proc :
-            .user
+        self.type = KernelEventType(rawValue: event.filter)
         
         self.userData = .pointer(event.udata)
         
@@ -196,306 +210,7 @@ public extension xlibc.kevent {
                     data: timeout, udata: udata)
     }
 }
-    
-public struct KernelEventType: RawRepresentable, Equatable {
-    
-    public typealias RawValue = Int16
-    public var rawValue: Int16
-    public init(rawValue: Int16) {
-        self.rawValue = rawValue
-    }
-    
-    public init(rawValue: Int32) {
-        self.rawValue = Int16(rawValue)
-    }
-    
-    public static func ==(lhs: KernelEventType, rhs: KernelEventType) -> Bool {
-        return lhs.rawValue == rhs.rawValue
-    }
-    
-    /// Takes a file descriptor as the identifier, and returns whenever there
-    /// is data available to read. The behavior of the filter is slightly
-    /// different depending on descriptor type.
-    ///
-    /// *Sockets*
-    ///
-    /// Socket which have previously been passed to `listen()` return when there
-    /// is an incoming connection pending. `data` contains the size of the listen
-    /// backlog.
-    /// Other socket descriptors return when there is data to be read, subject to
-    /// the SO_RCVLOWAT value of the socket buffer. This maybe overridden with a
-    /// per-filter low water mark at the time the filter is added by setting the
-    /// `NOTE_LOWAT` flag in `fflags`, and specifying the new low water mark in
-    /// `data`. On return `data` contains the number of bytes of protocol data
-    /// available to read.
-    /// If the read direction of the socket has shutdown, then the filter also sets
-    /// `EV_EOF` in `flags`, and returns the socket error (if any) in fflags. It is
-    /// possible for EOF to be returned (indicating the connection is gone) while
-    /// there is still data pending in the socket buffer.
-    ///
-    /// *Vnodes*
-    ///
-    /// Returns when the file pointer is not at the end of file. `data` contains the
-    /// offset from current position to end of file, and may be negative.
-    /// This behavior is different from `poll`, where read events are triggered for
-    /// regular files unconditionally. This event can be triggered unconditionally by
-    /// setting the `NOTE_FILE_POLL` (FreeBSD) flag in `fflags`.
-    ///
-    /// *Fifos, Pipes*
-    ///
-    /// Returns when the there is data to read; `data` contains the numener of bytes
-    /// available.
-    /// When the last writer disconnects, the filter will set EV_EOF in `flags`. This
-    /// may be cleared by passing in `EV_CLEAR`, at which point the filter will resume
-    /// waiting for data to become available before returning.
-    ///
-    /// *Device nodes (OS X)*
-    ///
-    /// Returns when there is data to read from the device; `data` contains the number
-    /// of bytes available. If the device does not support returning number of bytes, it
-    /// will not allow the filter to be attached. However, if the `NOTE_LOWAT` flag is
-    /// specified and the `data` field contains 1 on input, those devices will attach
-    /// count of bytes to read on output.
-    ///
-    /// *BPF devices (FreeBSD)*
-    ///
-    /// Returns when the BPF buffer is full, the `BPF` timeout has expired, or when the BPF
-    /// has `immediate mode` enabled and these is any data to read; `data` contains the
-    /// number of bytes available.
-    public static let read = KernelEventType(rawValue: EVFILT_READ)
-    
-    /// Takes a file descriptor as the identifier, and returns whenever it is
-    /// possible to write to the descriptor. For sockets, pipes and fifos, `data`
-    /// will contain the amount of space remaining in the write buffer. The filter
-    /// will set `EV_EOF` when the reader disconnects, and for the fifo case, this
-    /// may be cleared by use of `EV_CLEAR`. Note that this filter is not supported
-    /// for vnodes.
-    ///
-    /// For sockets, the low water mark and socket error handling is identical to the
-    /// `EVFILT_READ` case
-    public static let write = KernelEventType(rawValue: EVFILT_WRITE)
-    
-    /// The sigevent portion of the AIO request is filled in, with `sigev_notify_kqueue`
-    /// containing the kevent flags which should be `EV_ONESHOT`, `EV_CLEAR`, or
-    /// `EV_DISPATCH`, `sigev_value` containing the udata value, and `sigev_notify` set
-    /// to `SIGEVKernelEventENT`. When the `aio_*()` system call is made, the event will be
-    /// registered with the specified kqueue, and the ident argument set to the
-    /// `struct aiocb` returned by the `aio_*()` system call. The filter returns under
-    /// the same conditions as `aio_error()`.
-    ///
-    /// Absolute useless in Darwin, aka Apple Platform since it's not implemented.
-    public static let aio = KernelEventType(rawValue: EVFILT_AIO)
-    
-    /// In Darwin, Returns when the file pointer is not at the end of file. `data` contains
-    /// the offset from current position to end of file, and maybe negative.
-    ///
-    /// In BSD, Takes a file descriptor as the identifier and the events to watch for in
-    /// `fflags`, and returns when one or more of the requested events occurs on the
-    /// descriptor. The events to monitor are:
-    ///
-    /// *NOTE_ATTRIB*
-    ///
-    /// The file referenced by the descriptor had its attributes changed.
-    ///
-    /// *NOTE_CLOSE*
-    ///
-    /// A file descriptor referencing the monitored file, was closed. The closed file
-    /// descriptor did not have write access.
-    ///
-    /// *NOTE_CLOSE_WRITE*
-    ///
-    /// A file descriptor referencing the monitored file, was closed. The closed file
-    /// descriptor has write access.
-    ///
-    /// This note, as well as `NOTE_CLOSE`, are not activated when files are closed forcibly
-    /// by `unmount` or `revoke`. Instead, `NOTE_REVOKE` is sent for such events.
-    ///
-    /// *NOTE_DELETE*
-    ///
-    /// The `unlink()` system call was called on the file referenced by the descriptor.
-    ///
-    /// *NOTE_EXTEND*
-    ///
-    /// For regular file, the file referenced by the descriptor was extended.
-    ///
-    /// For directory, reports that a directory entry was added or removed, as the result
-    /// of rename operation. The `NOTE_EXTEND` event is not reported when a name is changed
-    /// inside the directory.
-    ///
-    /// *NOTE_LINK*
-    ///
-    /// The link count on the file changed. In particular, the `NOTE_LINK` event is reported
-    /// if a subdirectory was created pr de;eted inside the directory referenced by the
-    /// descriptor.
-    ///
-    /// *NOTE_OPEN*
-    ///
-    /// The file referenced by the descriptor was opened.
-    ///
-    /// *NOTE_READ*
-    ///
-    /// A read occurred on the file referenced by the descriptor.
-    ///
-    /// *NOTE_RENAME*
-    ///
-    /// The file referenced by the descriptor was renamed
-    ///
-    /// *NOTE_REVOKE*
-    ///
-    /// Access to the file was revoked via `revoke(2)` or the underlying file system was
-    /// unmounted.
-    ///
-    /// *NOTE_WRITE*
-    ///
-    /// A write occurred on the file referenced by the descriptor.
-    ///
-    /// On return, `fflags` contains the events which triggered the filter.
-    public static let vnode = KernelEventType(rawValue: EVFILT_VNODE)
-    
-    
-    /// Takes the process ID to monitor as the identifier and the events to watch
-    /// for in `fflags`, and returns when the process performs one or more of the
-    /// requested events. If a process can normally see another process, it can
-    /// attach an event to it. The events to monitor are:
-    ///
-    /// *NOTE_EXIT*
-    ///
-    /// The process has exited. The exit status will be stored in `data`.
-    ///
-    /// *NOTE_FORK*
-    ///
-    /// The process created a child process via `fork()` or similar call.
-    ///
-    /// *NOTE_EXEC*
-    ///
-    /// The process executed a new process via `execve` or similar call.
-    ///
-    /// *NOTE_SIGNAL*
-    ///
-    /// The process was sent a signal. Status can be checked via waitpid or similar call.
-    public static let proc = KernelEventType(rawValue: EVFILT_PROC)
-    
-    
-    /// Takes the signal number to monitor as the identifier and returns when the
-    /// given signal is delivered to the process. This coexists with the `signal()`
-    /// and `sigaction()` facilities, and has a lower precedence. Only signals
-    /// sent to the process, not to a particular thread, will trigger and the
-    /// filter. The filter will record all attempts to deliver a signal to a
-    /// process, even if the signal has been marked as `SIG_IGN`. Event notification
-    /// happends before normal signal delivery processing. `data` returns the number
-    /// of times the signal has been generated since the last call to `kevent()`.
-    /// This filter automatically sets the `EV_CLEAR` flag internally.
-    public static let signal = KernelEventType(rawValue: EVFILT_SIGNAL)
-    
-    
-    /// Establishes an arbitrary timer identified by `ident`. When adding a timer,
-    /// `data` specifies the timeout period. The timer will be periodic unless `EV_ONESHOT`
-    /// is specified. On return, `data` contains the number of times the timeout has
-    /// expired since the last call to `kevent()`. This filter automatically sets
-    /// the `EV_CLEAR` flag internally. There is a system wide limit on the number
-    /// of timers which is controlled by the `kern.kq_calloutmax` sysctl (FreeBSD).
-    ///
-    /// *NOTE_SECONDS*
-    ///
-    /// `data` is in seconds.
-    ///
-    /// *NOTE_MSECONDS*
-    ///
-    /// `data` is in milliseconds.
-    ///
-    /// *NOTE_USECONDS*
-    ///
-    /// `data` is in microseconds.
-    ///
-    /// *NOTE_NSECONDS*
-    ///
-    /// `data` is in nanoseconds.
-    ///
-    /// *NOTE_ABSOLUTE (Darwin)*
-    ///
-    /// `data` is an absolute timeout
-    ///
-    /// *NOTE_CRITICAL (Darwin)*
-    ///
-    /// system makes a best effort to fire this timer as scheduled.
-    ///
-    /// *NOTE_BACKGROUND (Darwin)*
-    ///
-    /// system has extra leeway to coalesce this timer.
-    ///
-    /// *NOTE_LEEWAY (Darwin)*
-    ///
-    /// exit[1] holds user-supplied slop in deadline for timer coalescing.
-    ///
-    /// If `fflags` is not set, the default is milliseconds. On return, fflags
-    /// contains the events which triggered the filter.
-    public static let timer = KernelEventType(rawValue: EVFILT_TIMER)
-    
-    
-    /// Establishes a user event identified by `ident` which is not associated with
-    /// any kernel mechanism but is triggered by user level code. The lower 24 bits
-    /// of the fflags may be used for user defined flags and manipulated using the
-    /// following:
-    ///
-    /// *NOTE_FFNOP*
-    ///
-    /// Ignore the input fflags.
-    ///
-    /// *NOTE_FFAND*
-    ///
-    /// Bitwise AND fflags.
-    ///
-    /// *NOTE_FFCOPY*
-    ///
-    /// Copy `fflags`.
-    ///
-    /// *NOTE_FFCTRLMASK*
-    ///
-    /// Control mask for fflags
-    ///
-    /// *NOTE_FFLAGSMASK*
-    ///
-    /// User defined flag mask for fflags.
-    ///
-    /// A user event is triggered for output with the following:
-    ///
-    /// *NOTE_TRIFFER*
-    ///
-    /// Cause the event to be triffered/
-    public static let user = KernelEventType(rawValue: EVFILT_USER)
-    
-    #if os(OSX) || os(iOS) || os(watchOS) || os(tvOS)
-    /// Takes the name of a mach port, or port set, in `ident` and waits until a
-    /// message is enqueued on the port or port set. When a message is detected,
-    /// but not directly received by the kevent call, the name of the specific port
-    /// where the message is enqueued is returned in `data`. If `fflags` contains
-    /// `MACH_RCV_MSG`, the `ext[0]` and `ext[1]` flags are assumed to contain
-    /// a pointer to the buffer where the message is to be received and the size
-    /// of the receive buffer, respectively. If `MACH_RCV_MSG` is specified,
-    /// yet the buffer size in `ext[1]` is zero, The space for the buffer may be
-    /// carved out of the data_out area provided to `kevent_qos() if there is enough
-    /// space remaining there.
-    public static let machPort = KernelEventType(rawValue: EVFILT_MACHPORT)
-    
-    /// The following has not documented in either Darwin nor FreeBSD
-    //        public static let fileSystem = Filter(rawValue: EVFILT_FS)
-    //        public static let virtualMemory = Filter(rawValue: EVFILT_VM)
-    #endif
-    
-    
-    
-    /// Takes a descriptor as the identifier, and returns whenever one of the
-    /// specified exceptional conditions has occurred on the descriptor. Conditions
-    /// are specified in `fflags`. Currently, this filter can be used to monitor the
-    /// arrival of out-of-band data on a socket descriptor using the filter flag
-    /// `NOTE_OOB`
-    ///
-    /// If the read direction of the socket has shutdown, then the filter also set
-    /// `EV_EOF` in `flags`, and returns the socket error (if any) in `fflags`.
-    public static let exception = KernelEventType(rawValue: EVFILT_EXCEPT)
-    
-}
-    
+
 #if arch(x86_64) || arch(arm64)
 public enum KernelEventUserData {
     case int(Int)
@@ -699,4 +414,38 @@ public extension KernelEventFlags
     public typealias User = KernelEventUserFlags
     public typealias None = KernelEventFlagsNone
 }
+    
+#if os(OSX) || os(iOS) || os(watchOS) || os(tvOS)
+    
+    public extension KernelEventFlags
+    {
+        public typealias VM = KernelEventVMFlags
+        public typealias Except = KernelEventExceptFlags
+    }
+    
+    public struct KernelEventExceptFlags: KernelEventFlags, RawRepresentable {
+        public typealias RawValue = UInt32
+        public var rawValue: UInt32
+        public init(rawValue: UInt32) {
+            self.rawValue = rawValue
+        }
+        
+        public static let oob = KernelEventExceptFlags(rawValue: UInt32(NOTE_OOB))
+    }
+    
+    public struct KernelEventVMFlags: KernelEventFlags, RawRepresentable {
+        public typealias RawValue = UInt32
+        public var rawValue: UInt32
+        
+        public init(rawValue: Int32) {
+            self.rawValue = UInt32(rawValue)
+        }
+        
+        public init(rawValue: UInt32) {
+            self.rawValue = rawValue
+        }
+        
+        public static let pressure = KernelEventVMFlags(rawValue: NOTE_VM_PRESSURE)
+    }
+#endif
 #endif
