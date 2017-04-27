67b833a85dd7403199814fd9fafe6da296a0f615
Yuji <hatsuneyuji@yahoo.co.jp>
2017-04-26 22:17:37 -0700

    linux tests

--- Tests/CKitTests/CKitTests.swift
+++ Tests/CKitTests/CKitTests.swift
@@ -26,6 +26,14 @@ class CKitTests: XCTestCase {
 
 	static var allTests : [(String, (CKitTests) -> () throws -> Void)] {
     return [
+        ("dns", test_dns),
+        ("dns", test_dns0),
+        ("dns", test_dns1),
+        ("ip4", testIpv4),
+        ("ip6", testIpv6),
+        ("unixsock", testUnixDomain),
+        ("nonblk", test_read_nonblk)
+        
 //      ("init ipv4 SocketAddress", testIPv4Init),
 //      ("init ipv6 SocketAddress", testIPv6Init),
     ]
