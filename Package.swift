4716c80edf768e7d7fab1c122e21efbdc505b648
yuji <hatsuneyuji@yahoo.co.jp>
2017-05-28 02:10:23 -0700

    use xlibc on github

--- Package.swift
+++ Package.swift
@@ -3,7 +3,6 @@ import PackageDescription
 let package = Package(
     name: "CKit",
     dependencies: [
-       // .Package(url: "https://github.com/michael-yuji/xlibc.git", Version(0,0,3))
-       .Package(url: "../xlibc", Version(0,0,3))
+        .Package(url: "https://github.com/michael-yuji/xlibc.git", Version(0,0,2))
     ]
 )
