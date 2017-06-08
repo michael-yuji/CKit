a84bffc6836f8e8a376bb9db5bbf2f4aa0523edf
yuuji <yuuji@yuujis-MacBook-Pro.local>
2017-06-08 07:38:27 -0700

    fix a bug in network interface test that it could fail since linux loopback interface does not support multicast by default.

--- Tests/CKitTests/NetworkInterfaceTests.swift
+++ Tests/CKitTests/NetworkInterfaceTests.swift
@@ -6,7 +6,7 @@ extension CKitTests
     func test_loopback()
     {
         let loopbackInteface = NetworkInterface.interfaces.filter {
-            $0.isLoopback && $0.running && $0.supportMulticast
+            $0.isLoopback && $0.running
         }
 
         XCTAssertTrue(loopbackInteface.map{
