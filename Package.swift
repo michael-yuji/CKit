7f9f7967475bfab4e41d796edddf9aa41cfaa8aa
michael-yuji <michael-yuji@users.noreply.github.com>
2020-04-09 01:43:28 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,11 +3,11 @@ import PackageDescription
 
 let package = Package(
     name: "CKit",
-    products: [ .library(name: "CKit", type: .dynamic, targets: ["CKit"]) ],
     platforms: [
       .macOS(.v10_10),
       .iOS(.v8)
     ],
+    products: [ .library(name: "CKit", type: .dynamic, targets: ["CKit"]) ],
     dependencies: [
         .package(url: "https://github.com/michael-yuji/xlibc", from: "0.0.3")
     ],
